80487a3e9fb2ab0a0f3749e8fdbafe8133c78295
tryup <fobnn2012@gmail.com>
2017-03-07 15:39:47 +0800

    remove backdoor

--- footer.php
+++ footer.php
@@ -9,19 +9,5 @@
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/util.min.js"></script> 
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/nav.js"></script>
 <?php wp_footer(); ?>
-<script>
-(function(){
-    var bp = document.createElement('script');
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https') {
-        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
-    }
-    else {
-        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-    }
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
-})();
-</script>
 </body>
 </html>
