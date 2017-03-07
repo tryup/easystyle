d30a2a5452e390c9126008077374dd565821db79
fobnn <fobnn2012@gmail.com>
2017-03-07 23:09:37 +0800

    fix issus on get tongji code

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer id="footer">
   <div class="copyright">
-    <p><i class="fa fa-copyright" aria-hidden="true"></i> <?php echo get_option('tang_years'); ?> <b><?php bloginfo('name'); ?></b></p>
+    <p><i class="fa fa-copyright" aria-hidden="true"></i> <?php echo fobnn_get_option('years'); ?> <b><?php bloginfo('name'); ?></b></p>
     <p>Powered by <b>WordPress</b>. | <?php echo get_option( 'zh_cn_l10n_icp_num' );?></p>
   </div>
-  <div style="display:none;"> <?php echo stripslashes(get_option('tang_tongji')); ?> </div>
+  <div style="display:none;"> <?php echo stripslashes(fobnn_get_option('tongji')); ?> </div>
 </footer>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/skel.min.js"></script> 
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/util.min.js"></script> 
